7c6ce18bf0eebf4e9cc07bc61559f0d94d249019
leedaeun <delee2008@gmail.com>
2023-05-17 16:55:50 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-console */
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { EventObject, ExternalEventTypes, Options } from '@toast-ui/calendar';
+import { TZDate } from '@toast-ui/calendar';
+import { ChangeEvent, MouseEvent } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+import Calendar from '@toast-ui/react-calendar';
+import { theme } from './Calendar/theme';
+import { addDate, addHours, subtractDate } from './Calendar/utils';
+
+import 'tui-date-picker/dist/tui-date-picker.css';
+import 'tui-time-picker/dist/tui-time-picker.css';
+
+const today = new TZDate();
+
+export function App({ view }) {
+  
+  const calendarRef = useRef(null);
+  const [selectedDateRangeText, setSelectedDateRangeText] = useState('');
+  const [selectedView, setSelectedView] = useState(view);
+  const initialCalendars= [
+    {
+      id: '0',
+      name: 'Private',
+      backgroundColor: '#9e5fff',
+      borderColor: '#9e5fff',
+      dragBackgroundColor: '#9e5fff',
+    },
+    {
+      id: '1',
+      name: 'Company',
+      backgroundColor: '#00a9ff',
+      borderColor: '#00a9ff',
+      dragBackgroundColor: '#00a9ff',
+    },
+  ];
+  
+
+  const getCalInstance = useCallback(() => calendarRef.current?.getInstance?.(), []);
+
+  const updateRenderRangeText = useCallback(() => {
+    const calInstance = getCalInstance();
+    if (!calInstance) {
+      setSelectedDateRangeText('');
+    }
+  
+    const calDate = calInstance.getDate();
+
+    let year = calDate.getFullYear();
+    let month = calDate.getMonth() + 1;
+    let dateRangeText = `${year}-${month}`;
+
+    setSelectedDateRangeText(dateRangeText);
+    }, [getCalInstance]
   );
-}
 
-export default App;
+
+    useEffect(() => {
+      setSelectedView(view);
+    }, [view]);
+
+
+  
+    useEffect(() => {
+      updateRenderRangeText();
+    }, [selectedView, updateRenderRangeText]);
+
+
+
+    const onBeforeDeleteEvent = (res) => {
+      const { id, calendarId } = res;
+      getCalInstance().deleteEvent(id, calendarId);
+    };
+
+    const onClickNavi = (ev) => {
+      if (ev.target.tagName === 'BUTTON') {
+      const button = ev.target;
+      const actionName = button.getAttribute('data-action').replace('move-', '');
+      getCalInstance()[actionName]();
+      updateRenderRangeText();
+      }
+    };
+
+    const onBeforeUpdateEvent = (updateData) => {
+      const targetEvent = updateData.event;
+      const changes = { ...updateData.changes };
+      getCalInstance().updateEvent(targetEvent.id, targetEvent.calendarId, changes);
+    };
+
+    const onBeforeCreateEvent = (eventData) => {
+      
+      const event = {
+
+      calendarId: eventData.calendarId || '',
+      id: String(Math.random()),
+      title: eventData.title,
+      isAllday: eventData.isAllday,
+      start: eventData.start,
+      end: eventData.end,
+      category: eventData.isAllday ? 'allday' : 'time',
+      dueDateClass: '',
+      location: eventData.location,
+      state: eventData.state,
+      isPrivate: eventData.isPrivate
+      };
+
+      getCalInstance().createEvents([event]);
+    };
+
+    return (
+      <div>
+        <h1>📅 Calendar</h1>
+      <div>
+
+      <span>
+        <button
+                type="button"
+                className="btn"
+                data-action="move-today"
+                onClick={onClickNavi}
+        >
+                Today
+        </button>
+        <button
+                type="button"
+                className="btn"
+                data-action="move-prev"
+                onClick={onClickNavi}
+        >
+                Prev
+        </button>
+        <button
+                type="button"
+                className="btn"
+                data-action="move-next"
+                onClick={onClickNavi}
+        >
+                Next
+        </button>
+      </span>
+
+      <span className="render-range">{selectedDateRangeText}</span>
+      
+      </div>
+        <Calendar
+          height="600px"
+
+         calendars={initialCalendars}
+
+          month={{ startDayOfWeek: 1, isAlways6Weeks: false}}
+
+          theme={theme} //테마옵션
+
+          useDetailPopup={true}
+          useFormPopup={true}
+          
+          view={selectedView}
+
+          ref={calendarRef}
+
+          onBeforeDeleteEvent={onBeforeDeleteEvent}
+          onBeforeUpdateEvent={onBeforeUpdateEvent}
+          onBeforeCreateEvent={onBeforeCreateEvent}
+        />
+      </div>
+    );
+  }    
---
+++ src/Calendar/theme.js
@@ -0,0 +1,27 @@
+export const theme = {
+    common: {
+      border: '1px solid #ddd',
+      backgroundColor: 'white',
+      holiday: { color: '#f54f3d' },
+      saturday: { color: '#135de6' },
+      dayName: { color: '#333' },
+      today: { color: '#fff' },
+      gridSelection: {
+        backgroundColor: 'rgba(19, 93, 230, 0.1)',
+        border: '1px solid #135de6',
+      },
+    },
+
+    month: {
+      dayName: {
+        borderLeft: 'none',
+        backgroundColor: 'inherit',
+      },
+      holidayExceptThisMonth: { color: '#f3acac' },
+      dayExceptThisMonth: { color: '#bbb' },
+      weekend: { backgroundColor: '#fafafa' },
+      moreView: { boxShadow: 'none' },
+      moreViewTitle: { backgroundColor: '#f4f4f4' },
+    },
+  };
+  
