79e575f874c5d0530c0d9a3169eaf59423434fe0
leedaeun <delee2008@gmail.com>
2023-06-13 20:21:15 +0900

    todo 수정

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -14,6 +14,7 @@ export const addUser = async (userInfo) => {
     }
   }
 };
+
 export const authUser = async (userInfo) => {
   try {
     const response = await axios.post(`${SERVER}/api/login`, userInfo);
@@ -22,7 +23,6 @@ export const authUser = async (userInfo) => {
       const token = response.data.token;
 
       localStorage.setItem("accessToken", token);
-
       const config = {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -34,5 +34,85 @@ export const authUser = async (userInfo) => {
   } catch (error) {
     console.log(error);
     throw error;
+  }
+};
+
+// todo 저장
+export const addTodo = async (todoitem) => {
+  try {
+    const token = localStorage.getItem("accessToken");
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`, // 인증 토큰을 헤더에 추가
+      },
+    };
+    const response = await axios.post(`${SERVER}/api/todo`, todoitem, config);
+    if (response.status === 200) {
+      alert("투두가 생성되었습니다.");
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error("Add todo error");
+  }
+};
+
+// 투두 삭제
+export const deleteTodo = async (todoId) => {
+  try {
+    const token = localStorage.getItem("accessToken");
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`, // 인증 토큰을 헤더에 추가
+      },
+    };
+    const response = await axios.delete(`${SERVER}/api/todo/${todoId}`, config);
+    if (response.status === 200) {
+      alert("투두가 삭제되었습니다.");
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error("delete folder error");
+  }
+};
+
+// 투두 목록 조회
+export const getTodo = async (date) => {
+  try {
+    const token = localStorage.getItem("accessToken");
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`, // 인증 토큰을 헤더에 추가
+      },
+    };
+    const response = await axios.get(`${SERVER}/api/todo/${date}`, config);
+    if (response.status === 200) {
+      alert("투두가 조회되었습니다.");
+      return response.data;
+    }
+  } catch (err) {
+    throw new Error("fetch department error");
+  }
+};
+
+//투두 수정
+export const updateTodo = async (todoId, updatedTodo) => {
+  try {
+    const token = localStorage.getItem("accessToken");
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`, // 인증 토큰을 헤더에 추가
+      },
+    };
+    const response = await axios.put(
+      `/api/todos/${todoId}`,
+      updatedTodo,
+      config
+    );
+    if (response.status === 200) {
+      alert("투두가 수정되었습니다.");
+      return response.data;
+    }
+  } catch (error) {
+    throw new Error("Failed to update todo");
   }
 };
